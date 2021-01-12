c398fa6da91eb8b95ecc1fd92008e0ac3f42c81b
felikowski <f.gloeckner@gmx.net>
2021-01-12 22:38:59 +0100

    connected backend with frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+var cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('This will be the future backend of the foodplan app');
 });
--- frontend/foodplan/src/app/app.component.ts
+++ frontend/foodplan/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'foodplan';
+  httpResponse: String;
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.http.get('http://localhost:3000', { responseType : 'text'}).subscribe(result => this.httpResponse = result);
+  }
 }

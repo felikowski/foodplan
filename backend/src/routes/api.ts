9353143a3a7f09fe14740aad6c31f6010946d13d
felikowski <f.gloeckner@gmx.net>
2021-12-17 23:41:31 +0100

    [FOODPLAN-22] created global logging service

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,37 +1,22 @@
 import express from 'express';
 import { dbWrapper } from '../lib/dbWrapper';
-import { ILogObject, Logger } from 'tslog';
+import { Log }from '../lib/Log';
 
-function logToDb(logObject: ILogObject) {
-  console.log(logObject.date, logObject.logLevel);
-  console.log(JSON.stringify(logObject));
-  dbWrapper.sendToLog(logObject);
-}
-const log: Logger = new Logger({ name: "logger" });
-log.attachTransport({
-  silly: logToDb,
-  debug: logToDb,
-  trace: logToDb,
-  info: logToDb,
-  warn: logToDb,
-  error: logToDb,
-  fatal: logToDb
-});
 const router = express.Router();
+router.use((req, res, next) => {
+  Log.getLog().silly(req.url);
+  next();
+});
 router.get('/', (req, res) => {
   res.send('api works');
 });
 router.get('/recipes', async (req, res) => {
-  console.log(req.url);
-  log.silly(req.url);
   const response: String = await dbWrapper.getAllRecipes();
   res.setHeader('content-type', 'application/json')
   res.send(response);
 })
 
 router.get('/recipes/:id', async (req, res) => {
-  const log: Logger = new Logger({ name: "logger" });
-  log.silly(req.url);
   const response: String = await dbWrapper.getSingleRecipe(+req.params.id);
   res.setHeader('content-type', 'application/json');
   res.send(response);

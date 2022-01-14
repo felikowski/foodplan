b73de58a2faf2bcd316c85edbd2d2ae8f7f8c8c8
felikowski <f.gloeckner@gmx.net>
2022-01-14 21:52:30 +0100

    [FOODPLAN-25] eslint for backend

--- backend/src/lib/Log.ts
+++ backend/src/lib/Log.ts
@@ -1,38 +1,41 @@
 import { ILogObject, Logger } from 'tslog';
-import config from '../../config.json';
 import got from 'got';
-export class Log {
-    private static log: Logger
-    public static getLog(): Logger {
-        if(!this.log) {
-            this.initialize();
-        }
-        return this.log;
-    }
-    static async sendToLog(logObject: ILogObject) {
-        const databaseUrl = `${config.database_webserver.url}:${config.database_webserver.port}`;
-        const dbLog = {
-            timestamp: logObject.date,
-            logLevel: logObject.logLevel,
-            message: logObject.argumentsArray,
-            codeline: `${logObject.fullFilePath} ${logObject.functionName} ${logObject.columnNumber}` 
-        };
-        const response = await got.post(`${databaseUrl}/log`, { json: dbLog}).json();
-    }
-    public static initialize() {
-        const logToDb = function (logObject: ILogObject) {
-            Log.sendToLog(logObject);
-        }
-        Log.log = new Logger({ name: "logger" });
-        Log.log.attachTransport({
-            silly: logToDb,
-            debug: logToDb,
-            trace: logToDb,
-            info: logToDb,
-            warn: logToDb,
-            error: logToDb,
-            fatal: logToDb
-        });
+import config from '../../config.json';
 
+export default class Log {
+  private static log: Logger;
+
+  public static getLog(): Logger {
+    if (!this.log) {
+      this.initialize();
     }
+    return this.log;
+  }
+
+  static async sendToLog(logObject: ILogObject) {
+    const databaseUrl = `${config.database_webserver.url}:${config.database_webserver.port}`;
+    const dbLog = {
+      timestamp: logObject.date,
+      logLevel: logObject.logLevel,
+      message: logObject.argumentsArray,
+      codeline: `${logObject.fullFilePath} ${logObject.functionName} ${logObject.columnNumber}`,
+    };
+    await got.post(`${databaseUrl}/log`, { json: dbLog }).json();
+  }
+
+  public static initialize() {
+    const logToDb = async function logToDb(logObject: ILogObject) {
+      await Log.sendToLog(logObject);
+    };
+    Log.log = new Logger({ name: 'logger' });
+    Log.log.attachTransport({
+      silly: logToDb,
+      debug: logToDb,
+      trace: logToDb,
+      info: logToDb,
+      warn: logToDb,
+      error: logToDb,
+      fatal: logToDb,
+    });
+  }
 }
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { Recipe } from '../lib/Recipe';
-import { Log }from '../lib/Log';
+import Recipe from '../lib/recipe';
+import Log from '../lib/Log';
 import { Ingredient } from '../lib/Ingredient';
 
 const router = express.Router();
@@ -14,63 +14,63 @@ router.get('/', (req, res) => {
 });
 
 router.get('/recipes', async (req, res) => {
-  const response: String = await Recipe.getAllRecipes();
-  res.setHeader('content-type', 'application/json')
+  const response: string = await Recipe.getAllRecipes();
+  res.setHeader('content-type', 'application/json');
   res.send(response);
-})
+});
 
 router.get('/recipes/:id', async (req, res) => {
-  const response: String = await Recipe.getSingleRecipe(+req.params.id);
+  const response: string = await Recipe.getSingleRecipe(+req.params.id);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.delete('/recipes/:id', async (req, res) => {
-  const response: String = await Recipe.deleteSingleRecipe(+req.params.id);
+  const response: string = await Recipe.deleteSingleRecipe(+req.params.id);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.post('/recipes/', async (req, res) => {
-  const response: String = await Recipe.insertRecipe(req.body);
+  const response: string = await Recipe.insertRecipe(req.body);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.post('/recipes/:id', async (req, res) => {
-  const response: String = await Recipe.updateRecipe(+req.params.id, req.body);
+  const response: string = await Recipe.updateRecipe(+req.params.id, req.body);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.get('/ingredients', async (req, res) => {
-  const response: String = await Ingredient.getAllIngredients();
-  res.setHeader('content-type', 'application/json')
+  const response: string = await Ingredient.getAllIngredients();
+  res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.get('/ingredients/:id', async (req, res) => {
-  const response: String = await Ingredient.getSingleIngredient(+req.params.id);
+  const response: string = await Ingredient.getSingleIngredient(+req.params.id);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.delete('/ingredients/:id', async (req, res) => {
-  const response: String = await Ingredient.deleteSingleIngredient(+req.params.id);
+  const response: string = await Ingredient.deleteSingleIngredient(+req.params.id);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.post('/ingredients', async (req, res) => {
-  const response: String = await Ingredient.insertIngredient(req.body);
+  const response: string = await Ingredient.insertIngredient(req.body);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
 router.post('/ingredients/:id', async (req, res) => {
-  const response: String = await Ingredient.updateIngredient(+req.params.id, req.body);
+  const response: string = await Ingredient.updateIngredient(+req.params.id, req.body);
   res.setHeader('content-type', 'application/json');
   res.send(response);
 });
 
-module.exports = router;
+export default router;
